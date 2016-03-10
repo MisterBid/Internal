ffac65050fdbed0988fcf6924f142bae665e6359
RafDevRBLX <RafDevRBLX@users.noreply.github.com>
2016-03-10 07:57:35 +0000

    Update

--- ArrowGames_361614747/Master.lua
+++ ArrowGames_361614747/Master.lua
@@ -68,10 +68,9 @@ local Admins = {
 	['1135681'] = 'SimplyPixel',
 	['10469346'] = 'chillybagpiper',
 	['55305559'] = 'Fireball1850',
-	['75750606'] = 'figuretjetsalt',
-	['32032357'] = 'Scriptey',
-	['25307609'] = 'dwong',
-	['62389100'] = 'Jacob_E',--temp
+	--['75750606'] = 'figuretjetsalt',
+	--['32032357'] = 'Scriptey',
+	--['25307609'] = 'dwong',
 }
 local debug = true
 local DataStore = game:GetService('DataStoreService'):GetDataStore(====1====)
